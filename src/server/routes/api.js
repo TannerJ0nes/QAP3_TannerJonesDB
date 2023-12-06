2e51ce1b7332893d5a3a03fad4ac8569748d0fb5
TannerJ0nes <127117143+TannerJ0nes@users.noreply.github.com>
2023-12-05 21:45:36 -0330

    Completed Phase 6

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -2,6 +2,18 @@
 const express = require('express');
 const router = express.Router();
 
-// Define API routes here
+// Mock data array for testing API routes
+const mockGameData = [
+  { id: 1, title: 'Game A', platform: 'PS4' },
+  { id: 2, title: 'Game B', platform: 'Xbox One' },
+  // Add more mock data as needed
+];
+
+// API routes
+router.get('/games', (req, res) => {
+  res.json(mockGameData);
+});
+
+// Add other API routes as needed
 
 module.exports = router;
--- src/server/routes/views.js
+++ src/server/routes/views.js
@@ -2,6 +2,18 @@
 const express = require('express');
 const router = express.Router();
 
-// Define view routes here
+// Mock data array for testing view routes
+const mockGameData = [
+  { id: 1, title: 'Game A', platform: 'PS4' },
+  { id: 2, title: 'Game B', platform: 'Xbox One' },
+  // Add more mock data as needed
+];
+
+// View routes
+router.get('/', (req, res) => {
+  res.render('index', { games: mockGameData });
+});
+
+// Add other view routes as needed
 
 module.exports = router;
